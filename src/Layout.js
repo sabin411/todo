1a725f2e544deac74b2b42c38c34bdcc6e21ab28
iamsabinstha <sherasabin411@gmail.com>
2021-10-06 15:07:38 +0545

    ✨ FIX #6 added route to other pages

--- src/Layout.js
+++ src/Layout.js
@@ -1,8 +1,11 @@
 import React from "react";
+import NotFound from "./Content/404";
 import CreateTodos from "./Content/CreateTodos";
 import TodoListComp from "./Content/TodoListComp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./Layout.css";
+import EditTodo from "./Content/EditTodo";
+import DetailsPage from "./Content/DetailsPage";
 function Layout() {
   return (
     <Router>
@@ -13,6 +16,15 @@ function Layout() {
           </Route>
           <Route path="/create-todos">
             <CreateTodos />
+          </Route>
+          <Route path="/update/:id">
+            <EditTodo />
+          </Route>
+          <Route path="/view/:id">
+            <DetailsPage />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
